f205f9e79dde7b78a189deb153b730df390ee57c
Facundo Uferer <juanfacundouf@gmail.com>
2023-05-29 22:21:41 -0300

    control de flujo

--- Level02/clase01/main.js
+++ Level02/clase01/main.js
@@ -1,24 +1,11 @@
-let num1 = 2;
-let num2 = 2;
-let num3 = 3;
+let ingreso = prompt("Ingrese la palabra");
 
-
-if (num1 >= num2 && num1 >= num3) {
-    if (num2 >= num3) {
-        console.log(`Orden: ${num1}, ${num2}, ${num3}`);
-    } else {
-        console.log(`Orden: ${num1}, ${num3}, ${num2}`);
-    }
-} else if (num2 >= num1 && num2 >= num3) {
-    if (num1 >= num3) {
-        console.log(`Orden: ${num2}, ${num1}, ${num3}`);
-    } else {
-        console.log(`Orden: ${num2}, ${num3}, ${num1}`);
-    }
+if (ingreso == "Hola") {
+    console.log("Hola")
+} else if (ingreso == "HOLA") {
+    console.log("HOLA");
+} else if (ingreso == "hola") {
+    console.log("hola")
 } else {
-    if (num1 >= num2) {
-        console.log(`Orden: ${num3}, ${num1}, ${num2}`);
-    } else {
-        console.log(`Orden: ${num3}, ${num2}, ${num1}`);
-    }
+    console.log("ni Hola, ni HOLA");
 }
