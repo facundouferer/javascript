826d753e7d2f964ece2bc4cc2d86d24089afcce5
Facundo Uferer <juanfacundouf@gmail.com>
2023-05-29 22:14:23 -0300

    condicionales

--- Level02/clase01/main.js
+++ Level02/clase01/main.js
@@ -1,7 +1,24 @@
-let numero = 10;
+let num1 = 2;
+let num2 = 2;
+let num3 = 3;
 
-let variable = numero > 10;
 
-if (numero == 10) {
-    alert("El numero es 10");
+if (num1 >= num2 && num1 >= num3) {
+    if (num2 >= num3) {
+        console.log(`Orden: ${num1}, ${num2}, ${num3}`);
+    } else {
+        console.log(`Orden: ${num1}, ${num3}, ${num2}`);
+    }
+} else if (num2 >= num1 && num2 >= num3) {
+    if (num1 >= num3) {
+        console.log(`Orden: ${num2}, ${num1}, ${num3}`);
+    } else {
+        console.log(`Orden: ${num2}, ${num3}, ${num1}`);
+    }
+} else {
+    if (num1 >= num2) {
+        console.log(`Orden: ${num3}, ${num1}, ${num2}`);
+    } else {
+        console.log(`Orden: ${num3}, ${num2}, ${num1}`);
+    }
 }
